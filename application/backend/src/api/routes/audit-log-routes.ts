faf8777d53a0444f331733c91571e8764c1b6c17
Marko Malenic <mmalenic1@gmail.com>
2022-10-19 15:41:46 +1100

    Get entries for audit log service in routes

--- application/backend/src/api/routes/audit-log-routes.ts
+++ application/backend/src/api/routes/audit-log-routes.ts
@@ -8,9 +8,9 @@ import * as edgedb from "edgedb";
 import { container } from "tsyringe";
 import { AuditLogService } from "../../business/services/audit-log-service";
 
-export const auditLogRoutes = async (fastify: FastifyInstance, opts: any) => {
+export const auditLogRoutes = async (fastify: FastifyInstance, _opts: any) => {
   const edgeDbClient = container.resolve<edgedb.Client>("Database");
-  //const auditLogService = container.resolve<AuditLogService>(AuditLogService);
+  const auditLogService = container.resolve<AuditLogService>(AuditLogService);
 
   fastify.get<{ Params: { rid: string }; Reply: AuditEntryType[] }>(
     "/api/releases/:rid/audit-log",
@@ -21,7 +21,7 @@ export const auditLogRoutes = async (fastify: FastifyInstance, opts: any) => {
 
       const releaseId = request.params.rid;
 
-      /* const events = await auditLogService.getEntries(
+      const events = await auditLogService.getEntries(
         edgeDbClient,
         authenticatedUser,
         releaseId,
@@ -34,7 +34,7 @@ export const auditLogRoutes = async (fastify: FastifyInstance, opts: any) => {
         events,
         page,
         `/api/releases/${releaseId}/audit-log?`
-      ); */
+      );
     }
   );
 };
