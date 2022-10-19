cafe60b662a309a85d9f34a27ea3e38fc75d59d4
Marko Malenic <mmalenic1@gmail.com>
2022-10-19 15:55:14 +1100

    Add additional fields to audit schema

--- application/common/elsa-types/schemas-audit.ts
+++ application/common/elsa-types/schemas-audit.ts
@@ -2,14 +2,15 @@ import { Static, Type } from "@sinclair/typebox";
 import { TypeDate } from "./typebox-helpers";
 
 export const AuditEntrySchema = Type.Object({
+  whoId: Type.String(),
   whoDisplay: Type.String(),
-
   actionCategory: Type.String(),
   actionDescription: Type.String(),
-
+  recordedDateTime: TypeDate,
+  updatedDateTime: TypeDate,
   when: TypeDate,
-
   duration: Type.Optional(Type.String()),
+  outcome: Type.Integer(),
 });
 
 export type AuditEntryType = Static<typeof AuditEntrySchema>;
