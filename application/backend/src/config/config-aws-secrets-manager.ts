b0459de3e4ccf01f754df5a2cd0b3006be6e6ce5
andrewpatto <andrew@patto.net>
2022-11-08 15:54:39 +1100

    Config management to support different AWS deploys (will be fixed by meta config mechanism)

--- application/backend/src/config/config-aws-secrets-manager.ts
+++ application/backend/src/config/config-aws-secrets-manager.ts
@@ -4,19 +4,18 @@ import {
 } from "@aws-sdk/client-secrets-manager";
 import { set } from "lodash";
 
-// TODO: should this be some sort of parameter - overrideable? (I mean that makes it a meta setting - a
-// setting to find settings - so lets be careful before jumping it to that)
-const SECRET_ID = "Elsa";
-
 /**
  * Converts a key/value secret in AWS SecretsManager into a JSON object that can be used for
  * merging in the configuration system.
  */
-export async function getConfigAwsSecretsManager() {
+export async function getConfigAwsSecretsManager(isLocal: boolean) {
   const client = new SecretsManagerClient({});
 
+  // TODO: should this be set by the meta config system - for the moment this will do
+  const secretId = isLocal ? "ElsaDataLocalhost" : "ElsaDataDevDeployed";
+
   const secretResult = await client.send(
-    new GetSecretValueCommand({ SecretId: SECRET_ID })
+    new GetSecretValueCommand({ SecretId: secretId })
   );
 
   if (secretResult.SecretBinary || !secretResult.SecretString) {
@@ -24,6 +23,8 @@ export async function getConfigAwsSecretsManager() {
       "We expect the Elsa secret to be a secret string (as JSON key/values) - not a SecretBinary"
     );
   }
+
+  console.log(`Loading configuration from AWS secret ${secretResult.ARN}`);
 
   // because we like to use the secrets manager UI - we record the values there
   // as a flat object of Key/value pairs (with dots in the key name to show nesting).
