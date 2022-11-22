92e35aaf63ecda1b0861486bc0ba700e021b96db
andrewpatto <andrew@patto.net>
2022-11-23 09:09:48 +1100

    execute db tests (currently empty)

--- application/backend/tests/db-tests/db.base.test.ts
+++ application/backend/tests/db-tests/db.base.test.ts
@@ -1,12 +1,7 @@
 import { Client, createClient } from "edgedb";
 import { blankTestData } from "../../src/test-data/blank-test-data";
-import { insert10G } from "../../src/test-data/insert-test-data-10g";
-import e from "../../dbschema/edgeql-js";
-import { registerTypes } from "../service-tests/setup";
 
-const testContainer = registerTypes();
-
-describe("edgedb base tests", () => {
+describe("edgedb tests", () => {
   let edgeDbClient: Client;
 
   beforeAll(async () => {
@@ -17,8 +12,11 @@ describe("edgedb base tests", () => {
 
   beforeEach(async () => {
     await blankTestData();
-    await insert10G();
   });
 
-  it("root page is some sort of HTML", async () => {});
+  it("can connect using the client for a basic select", async () => {
+    const result = await edgeDbClient.querySingle(`select 2 + 2;`);
+
+    expect(result).toBe(4);
+  });
 });
