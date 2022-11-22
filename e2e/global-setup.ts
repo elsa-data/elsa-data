c374527cbd5ff28f9f8ae1d49fd74aa6c8a744b4
andrewpatto <andrew@patto.net>
2022-11-23 08:20:02 +1100

    More fiddling

--- e2e/global-setup.ts
+++ e2e/global-setup.ts
@@ -1,5 +1,11 @@
 import { chromium, FullConfig } from "@playwright/test";
 
+/**
+ * The global setup for local testing uses our 'dev only' login
+ * page that allows direct button login for 3 different roles.
+ *
+ * @param config
+ */
 async function globalSetup(config: FullConfig) {
   const { baseURL, storageState } = config.projects[0].use;
   const browser = await chromium.launch();
