8cc8f8392869a86de6362340c45fe6952153b914
Andrew Patterson <andrew@patto.net>
2022-11-25 10:45:54 +1100

    Some fix ups to get the Docker deployed elsa working with meta config and commands (#128)

--- application/local_run.sh
+++ application/local_run.sh
@@ -1,13 +1,11 @@
 
-EDGEDB_CLIENT_SECURITY=insecure_dev_mode
-
 docker run -it --rm -e AWS_REGION \
         -e AWS_ACCESS_KEY_ID \
         -e AWS_SECRET_ACCESS_KEY \
         -e AWS_SESSION_TOKEN \
         -e NODE_ENV="development" \
         -e ELSA_DATA_META_CONFIG_SOURCES="file('base') file('dev-common') file('dev-localhost') aws-secret('ElsaDataLocalhost')" \
-        -e EDGEDB_CLIENT_SECURITY \
+        -e ELSA_DATA_CONFIG_PORT=80 \
         -e EDGEDB_DSN \
-        -e EDGEDB_PASSWORD \
         -p 3000:80 elsa
+
