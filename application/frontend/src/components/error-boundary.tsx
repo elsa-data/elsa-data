ddfa2d1f40110dadf8433908c5ea1daf289c31d8
Marko Malenic <mmalenic1@gmail.com>
2022-12-01 18:21:12 +1100

    Make error boundary smarter by detecting offline or incorrectly authenticated users

--- application/frontend/src/components/error-boundary.tsx
+++ application/frontend/src/components/error-boundary.tsx
@@ -1,23 +1,42 @@
-import React, { ReactNode } from "react";
+import { Component, ReactNode } from "react";
 import { Base7807Error } from "@umccr/elsa-types/error-types";
+import classNames from "classnames";
 import axios from "axios";
 
 export type ErrorDisplayProps = {
-  children? : React.ReactNode
+  children? : ReactNode
   message?: ReactNode;
   // Should this component be displayed even if no error has been thrown.
   displayEagerly?: boolean;
   error?: any;
+  styling?: string;
+  // Whether this error should be rethrown to a higher up component.
+  rethrowError: (error: any) => boolean;
 };
 
 export type ErrorDisplayState = {
   error?: any;
 };
 
+export type ErrorBoxProps = {
+  children?: ReactNode;
+  styling?: string;
+}
+
+export const ErrorBox = ({ children, styling }: ErrorBoxProps): JSX.Element => {
+  return <div className={classNames("p-4 flex place-content-center text-sm text-red-700 bg-red-100", styling)}>
+    {children}
+  </div>;
+}
+
 /**
  * Display an error message on throw.
  */
-export class ErrorBoundary extends React.Component<ErrorDisplayProps, ErrorDisplayState> {
+export class ErrorBoundary extends Component<ErrorDisplayProps, ErrorDisplayState> {
+  static defaultProps = {
+    rethrowError: (error: any) => !navigator.onLine || ErrorBoundary.isAuthenticationError(error)
+  }
+
   constructor(props: ErrorDisplayProps) {
     super(props);
     this.state = { error: props.error };
@@ -27,14 +46,16 @@ export class ErrorBoundary extends React.Component<ErrorDisplayProps, ErrorDispl
     return { error: error };
   }
 
-  formatError(): JSX.Element | undefined {
-    const error = this.state.error;
+  static isAuthenticationError(error: any): boolean {
+    return (error instanceof Base7807Error && error.status == 403) ||
+      (axios.isAxiosError(error) && error.code === "403");
+  }
+
+  static formatErrorDetail(error: any): JSX.Element | undefined {
     if (error !== undefined) {
       if (error instanceof Base7807Error) {
         const error7808 = error.toResponse();
         return <>
-          This could be an issue with the network, here are some details:
-          <br></br>
           <div>
             <span className="font-bold">type: </span>
             {error7808.type}
@@ -59,18 +80,20 @@ export class ErrorBoundary extends React.Component<ErrorDisplayProps, ErrorDispl
           </div>}
         </>;
       } else if (axios.isAxiosError(error)) {
-        return <>
-          This could be an issue with the network, here are some details:
-          <br></br>
-          <div>
-            <span className="font-bold">message: </span>
-            {error.message}
-          </div>
-          <div>
-            <span className="font-bold">code: </span>
-            {error.code}
-          </div>
-        </>;
+        if (error.response?.data instanceof Base7807Error) {
+          return this.formatErrorDetail(error.response.data);
+        } else {
+          return <>
+            <div>
+              <span className="font-bold">message: </span>
+              {error.message}
+            </div>
+            <div>
+              <span className="font-bold">code: </span>
+              {error.code}
+            </div>
+          </>;
+        }
       } else if (error instanceof Error) {
         return <div>{error.message}</div>;
       } else {
@@ -79,15 +102,34 @@ export class ErrorBoundary extends React.Component<ErrorDisplayProps, ErrorDispl
     }
   }
 
+  formatError() : JSX.Element | undefined {
+    if (ErrorBoundary.isAuthenticationError(this.state.error)) {
+      return <ErrorBox styling={this.props.styling}>
+        Failed to authenticate, check your crendentials.
+        <br></br>
+        Here are some details:
+        {this.state.error && ErrorBoundary.formatErrorDetail(this.state.error)}
+      </ErrorBox>;
+    } else {
+      return <ErrorBox styling={this.props.styling}>
+        {this.props.message ? <div>{this.props.message}</div> : <div>Something went wrong.</div>}
+        <br></br>
+        Here are some details:
+        {this.state.error && ErrorBoundary.formatErrorDetail(this.state.error)}
+      </ErrorBox>;
+    }
+  }
+
   render() {
-    if (this.props.displayEagerly || (this.state.error !== undefined && this.state.error !== null)) {
-      return (
-        <div className="p-4 text-sm text-red-700 bg-red-100">
-          {this.props.message ? <div>{this.props.message}</div> : <div>Something went wrong.</div>}
-          <br></br>
-          {this.state.error && this.formatError()}
-        </div>
-      );
+    if (!navigator.onLine) {
+      return <ErrorBox styling={this.props.styling}>
+        You are offline, check your connection.
+      </ErrorBox>;
+    } else if (this.props.displayEagerly || (this.state.error !== undefined && this.state.error !== null)) {
+      if (this.props.rethrowError(this.state.error)) {
+        throw this.state.error;
+      }
+      return this.formatError();
     } else {
       return this.props.children;
     }
--- application/frontend/src/layouts/layout-base.tsx
+++ application/frontend/src/layouts/layout-base.tsx
@@ -26,149 +26,147 @@ export const LayoutBase: React.FC<PropsWithChildren<Props>> = ({
     const whenInactive = `text-gray-500 border-white ${hoverClass} ${always}`;
 
     return (
-      <ErrorBoundary>
-        <li className="mr-6 my-2 md:my-0">
-          <NavLink
-            to={to}
-            className={({ isActive }) => (isActive ? whenActive : whenInactive)}
-          >
-            <span className="pb-1 md:pb-0 text-sm">{label}</span>
-          </NavLink>
-        </li>
-      </ErrorBoundary>
+      <li className="mr-6 my-2 md:my-0">
+        <NavLink
+          to={to}
+          className={({ isActive }) => (isActive ? whenActive : whenInactive)}
+        >
+          <span className="pb-1 md:pb-0 text-sm">{label}</span>
+        </NavLink>
+      </li>
     );
   };
 
   const loggedInUser = useLoggedInUser();
 
   return (
-    <ErrorBoundary>
-      <>
-        {/* NAV START */}
-        <nav
-          id="header"
-          className="bg-white fixed w-full z-10 top-0 shadow min-w-max"
-        >
-          <div className="w-full container mx-auto">
-            <div className="w-full justify-between flex flex-wrap items-center mt-0 pt-3 pb-3 lg:pb-0">
-              <div className="block lg:hidden pr-4">
-                <button
-                  onClick={() => setIsMenuBarOpen((prev: boolean) => !prev)}
-                  id="nav-toggle"
-                  className="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-gray-900 hover:border-teal-500 appearance-none focus:outline-none"
+    <>
+      {/* NAV START */}
+      <nav
+        id="header"
+        className="bg-white fixed w-full z-10 top-0 shadow min-w-max"
+      >
+        <div className="w-full container mx-auto">
+          <div className="w-full justify-between flex flex-wrap items-center mt-0 pt-3 pb-3 lg:pb-0">
+            <div className="block lg:hidden pr-4">
+              <button
+                onClick={() => setIsMenuBarOpen((prev: boolean) => !prev)}
+                id="nav-toggle"
+                className="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-gray-900 hover:border-teal-500 appearance-none focus:outline-none"
+              >
+                <svg
+                  className="fill-current h-3 w-3"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  <svg
-                    className="fill-current h-3 w-3"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <title>Menu</title>
-                    <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-                  </svg>
-                </button>
-              </div>
-
-              <div className="pl-2 md:pl-0">
-                <a
-                  className="text-gray-900 text-base xl:text-xl no-underline hover:no-underline font-bold"
-                  href="/"
-                >
-                  Elsa Data
-                </a>
-              </div>
-              <div className="flex relative float-right">
-                {loggedInUser && <LayoutBaseHeaderUser user={loggedInUser} />}
-              </div>
+                  <title>Menu</title>
+                  <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                </svg>
+              </button>
             </div>
 
-            <div
-              className={`w-full flex-grow lg:flex lg:items-center lg:w-auto mt-2 lg:mt-0 bg-white ${
-                isMenuBarOpen ? "" : "hidden"
-              }`}
-              id="nav-content"
-            >
-              {loggedInUser ? (
-                <ul className="list-reset lg:flex flex-1 items-center px-4 md:px-0 pb-4 lg:pb-0">
-                  <li className="mr-6 my-2 md:my-0">
-                    {navLink(
-                      "/",
-                      "Home",
-                      "text-orange-600",
-                      "border-orange-600",
-                      "hover:border-orange-600"
-                    )}
-                  </li>
-                  <li className="mr-6 my-2 md:my-0">
-                    {navLink(
-                      "/releases",
-                      "Releases",
-                      "text-purple-500",
-                      "border-purple-500",
-                      "hover:border-purple-500"
-                    )}
-                  </li>
-                  <li className="mr-6 my-2 md:my-0">
-                    {navLink(
-                      "/datasets",
-                      "Datasets",
-                      "text-green-500",
-                      "border-green-500",
-                      "hover:border-green-500"
-                    )}
-                  </li>
-                  <li className="mr-6 my-2 md:my-0">
-                    {navLink(
-                      "/dac",
-                      "DAC",
-                      "text-yellow-500",
-                      "border-yellow-500",
-                      "hover:border-yellow-500"
-                    )}
-                  </li>
-                  <li className="mr-6 my-2 md:my-0">
-                    {navLink(
-                      "/users",
-                      "Users",
-                      "text-rose-500",
-                      "border-rose-500",
-                      "hover:border-rose-500"
-                    )}
-                  </li>
-                </ul>
-              ) : (
-                <ul className="list-reset lg:flex flex-1 items-center px-4 md:px-0">
-                  <li className="mr-6 my-2 md:my-0">
-                    {navLink(
-                      "/",
-                      "Login",
-                      "text-gray-500",
-                      "border-gray-500",
-                      "hover:border-gray-500"
-                    )}
-                  </li>
-                </ul>
-              )}
-
-              {/*<div className="relative pull-right pl-4 pr-4 md:pr-0">
-                <input
-                  type="search"
-                  placeholder="Search"
-                  className="w-full bg-gray-100 text-sm text-gray-800 transition border focus:outline-none focus:border-gray-700 rounded py-1 px-2 pl-10 appearance-none leading-normal"
-                />
-              </div> */}
+            <div className="pl-2 md:pl-0">
+              <a
+                className="text-gray-900 text-base xl:text-xl no-underline hover:no-underline font-bold"
+                href="/"
+              >
+                Elsa Data
+              </a>
+            </div>
+            <div className="flex relative float-right">
+              {loggedInUser && <LayoutBaseHeaderUser user={loggedInUser} />}
             </div>
           </div>
-        </nav>
-        {/* NAV END */}
 
-        <div className="container w-full mx-auto pt-10 lg:pt-20 grow">
-          <div className="w-full mt-8 mb-8 text-gray-800 leading-normal">
-            {children}
+          <div
+            className={`w-full flex-grow lg:flex lg:items-center lg:w-auto mt-2 lg:mt-0 bg-white ${
+              isMenuBarOpen ? "" : "hidden"
+            }`}
+            id="nav-content"
+          >
+            {loggedInUser ? (
+              <ul className="list-reset lg:flex flex-1 items-center px-4 md:px-0 pb-4 lg:pb-0">
+                <li className="mr-6 my-2 md:my-0">
+                  {navLink(
+                    "/",
+                    "Home",
+                    "text-orange-600",
+                    "border-orange-600",
+                    "hover:border-orange-600"
+                  )}
+                </li>
+                <li className="mr-6 my-2 md:my-0">
+                  {navLink(
+                    "/releases",
+                    "Releases",
+                    "text-purple-500",
+                    "border-purple-500",
+                    "hover:border-purple-500"
+                  )}
+                </li>
+                <li className="mr-6 my-2 md:my-0">
+                  {navLink(
+                    "/datasets",
+                    "Datasets",
+                    "text-green-500",
+                    "border-green-500",
+                    "hover:border-green-500"
+                  )}
+                </li>
+                <li className="mr-6 my-2 md:my-0">
+                  {navLink(
+                    "/dac",
+                    "DAC",
+                    "text-yellow-500",
+                    "border-yellow-500",
+                    "hover:border-yellow-500"
+                  )}
+                </li>
+                <li className="mr-6 my-2 md:my-0">
+                  {navLink(
+                    "/users",
+                    "Users",
+                    "text-rose-500",
+                    "border-rose-500",
+                    "hover:border-rose-500"
+                  )}
+                </li>
+              </ul>
+            ) : (
+              <ul className="list-reset lg:flex flex-1 items-center px-4 md:px-0">
+                <li className="mr-6 my-2 md:my-0">
+                  {navLink(
+                    "/",
+                    "Login",
+                    "text-gray-500",
+                    "border-gray-500",
+                    "hover:border-gray-500"
+                  )}
+                </li>
+              </ul>
+            )}
+
+            {/*<div className="relative pull-right pl-4 pr-4 md:pr-0">
+              <input
+                type="search"
+                placeholder="Search"
+                className="w-full bg-gray-100 text-sm text-gray-800 transition border focus:outline-none focus:border-gray-700 rounded py-1 px-2 pl-10 appearance-none leading-normal"
+              />
+            </div> */}
           </div>
         </div>
+      </nav>
+      {/* NAV END */}
 
-        {/*<LayoutBaseFooter />*/}
-      </>
-    </ErrorBoundary>
+      <div className="container w-full mx-auto pt-10 lg:pt-20 grow">
+        <div className="w-full mt-8 mb-8 text-gray-800 leading-normal">
+          <ErrorBoundary rethrowError={(_: any) => false}>
+            {children}
+          </ErrorBoundary>
+        </div>
+      </div>
+
+      {/*<LayoutBaseFooter />*/}
+    </>
   );
 };
