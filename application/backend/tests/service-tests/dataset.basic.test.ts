5d8a658a77ffe0ef1a3ba6d33a1fe7d01eae2648
William Putra Intan <61998484+williamputraintan@users.noreply.github.com>
2022-12-02 12:34:47 +1100

    Update dataset.basic.test.ts

--- application/backend/tests/service-tests/dataset.basic.test.ts
+++ application/backend/tests/service-tests/dataset.basic.test.ts
@@ -43,18 +43,17 @@ it("basic summary get all has correct summary values for family dataset", async 
   expect(result.data.length).toBe(2);
 
   // because we get consistently sorted results (by uri) - we expect family dataset to be first
-  const family = result.data[0];
-  console.log("family", result.data);
+  const family = result.data[1];
 
-  expect(family.id).toBe(tenfDatasetId);
-  expect(family.uri).toBe(TENF_URI);
-  expect(family.description).toBe("UMCCR 10F");
-  expect(family.summaryArtifactCount).toBe(32);
+  expect(family.id).toBe(tengDatasetId2);
+  expect(family.uri).toBe(TENG_URI);
+  expect(family.description).toBe("UMCCR 10G");
+  expect(family.summaryArtifactCount).toBe(20);
   expect(family.summaryArtifactIncludes).toBe("BAM VCF");
-  expect(family.summaryArtifactSizeBytes).toBe(29532975421);
-  expect(family.summaryCaseCount).toBe(4);
-  expect(family.summaryPatientCount).toBe(16);
-  expect(family.summarySpecimenCount).toBe(16);
+  expect(family.summaryArtifactSizeBytes).toBe(546398018478);
+  expect(family.summaryCaseCount).toBe(10);
+  expect(family.summaryPatientCount).toBe(10);
+  expect(family.summarySpecimenCount).toBe(10);
 });
 
 it("basic summary get all is sorted by dataset URI", async () => {
