787b0e5e11cd7aa29554321b8c02823630b1a285
William Putra Intan <61998484+williamputraintan@users.noreply.github.com>
2022-12-09 18:34:51 +1100

    Create edgedb-schema.test.ts

---
+++ application/backend/tests/db-tests/edgedb-schema.test.ts
@@ -0,0 +1,86 @@
+import { Client, createClient } from "edgedb";
+import e from "../../dbschema/edgeql-js";
+import { blankTestData } from "../../src/test-data/blank-test-data";
+import { makeSystemlessIdentifierArray } from "../../src/test-data/test-data-helpers";
+
+describe("edgedb tests", () => {
+  let edgeDbClient: Client;
+
+  beforeAll(async () => {
+    edgeDbClient = createClient({});
+  });
+
+  afterAll(() => {});
+
+  beforeEach(async () => {
+    await blankTestData();
+  });
+
+  it("Check cascading dataset", async () => {
+    const insertArtifact = e.insert(e.lab.ArtifactBcl, {
+      bclFile: e.insert(e.storage.File, {
+        url: "s3://test-file.bcl",
+        size: 101,
+        checksums: [
+          {
+            type: "MD5",
+            value: "RANDOM_CHECKSUM",
+          },
+        ],
+      }),
+    });
+
+    const insertSpecimen = e.insert(e.dataset.DatasetSpecimen, {
+      externalIdentifiers: makeSystemlessIdentifierArray("specimen-id-test"),
+      artifacts: e.set(insertArtifact),
+    });
+
+    const insertPatient = e.insert(e.dataset.DatasetPatient, {
+      externalIdentifiers: makeSystemlessIdentifierArray("patient-id-test"),
+      consent: e.insert(e.consent.Consent, {}),
+      specimens: e.set(insertSpecimen),
+    });
+
+    const insertDataset = e.insert(e.dataset.Dataset, {
+      uri: "test-dataset-uri",
+      externalIdentifiers: makeSystemlessIdentifierArray(""),
+      description: "A uri test",
+      cases: e.set(
+        e.insert(e.dataset.DatasetCase, {
+          externalIdentifiers: makeSystemlessIdentifierArray("case-id-test"),
+          patients: e.set(insertPatient),
+        })
+      ),
+    });
+
+    await insertDataset.run(edgeDbClient);
+
+    const selectDataset = async () =>
+      await e.select(e.dataset.Dataset, () => ({})).run(edgeDbClient);
+    const selectPatient = async () =>
+      await e.select(e.dataset.DatasetPatient, () => ({})).run(edgeDbClient);
+    const selectSpecimen = async () =>
+      await e.select(e.dataset.DatasetSpecimen, () => ({})).run(edgeDbClient);
+    const selectFile = async () =>
+      await e.select(e.storage.File, () => ({})).run(edgeDbClient);
+    const selectConsent = async () =>
+      await e.select(e.consent.Consent, () => ({})).run(edgeDbClient);
+
+    // Making sure it exist
+    expect((await selectDataset()).length).toEqual(1);
+    expect((await selectPatient()).length).toEqual(1);
+    expect((await selectSpecimen()).length).toEqual(1);
+    expect((await selectFile()).length).toEqual(1);
+    expect((await selectConsent()).length).toEqual(1);
+
+    // Delete dataset to ensure all hierarchy deleted
+    await e.delete(e.dataset.Dataset).run(edgeDbClient);
+
+    // Expect all children is deleted as part of dataset deletion
+    expect((await selectDataset()).length).toEqual(0);
+    expect((await selectPatient()).length).toEqual(0);
+    expect((await selectSpecimen()).length).toEqual(0);
+    expect((await selectFile()).length).toEqual(0);
+    expect((await selectConsent()).length).toEqual(0);
+  });
+});
