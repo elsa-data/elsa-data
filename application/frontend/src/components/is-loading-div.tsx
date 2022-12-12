87ce5b46d247f2ca51b81b768535a714c8787cfe
andrewpatto <andrew@patto.net>
2022-12-12 16:19:16 +1100

    Added animated loading div

--- application/frontend/src/components/is-loading-div.tsx
+++ application/frontend/src/components/is-loading-div.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSnowflake } from "@fortawesome/free-regular-svg-icons";
 
 /**
  * A full width, some height dev showing a loading message
  */
 export const IsLoadingDiv = () => {
   return (
-    <div className="flex min-h-[10em] w-full items-center justify-center">
+    <div className="flex min-h-[10em] w-full flex-col items-center justify-center gap-8">
+      <FontAwesomeIcon
+        icon={faSnowflake}
+        size="3x"
+        spin={true}
+        color="#93c5fd"
+      />
       Loading...
     </div>
   );
