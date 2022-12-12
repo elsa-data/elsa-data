8fbdf607db76c03fa136335fd2563cea14ea5141
William Putra Intan <61998484+williamputraintan@users.noreply.github.com>
2022-12-12 14:46:07 +1100

    flowbite-react installation

--- application/frontend/tailwind.config.js
+++ application/frontend/tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/**/*.html"],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("flowbite/plugin"),
+  ],
 };
