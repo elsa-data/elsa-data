36d36fe3e613308b3447d073a2a3ad08f18c5c5a
Andrew Patterson <andrew@patto.net>
2023-01-09 12:57:35 +1100

    Moved infrastructure to dedicated repos (#182)

--- infrastructure/aws/tools/show-edge-db-password.sh
+++
@@ -1,7 +0,0 @@
-
-# This (temporary) tool can be used for outputting the current EdgeDb passwords from AWS
-
-SECRET=$(aws secretsmanager list-secrets --filter "Key=name,Values=EdgeDb" --query "SecretList[0].ARN" --output text)
-PW=$(aws secretsmanager get-secret-value --secret-id $SECRET --query "SecretString" --output text)
-
-echo $PW
