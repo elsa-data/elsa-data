aebc1f11c06b6710203b4dcdd2dbbf39563b84fe
Marko Malenic <mmalenic1@gmail.com>
2023-02-02 10:25:04 +1100

    test(backend): move user tests to their own file and fix asserts

---
+++ application/backend/tests/service-tests/audit-log-user.test.ts
@@ -0,0 +1,139 @@
+import { AuthenticatedUser } from "../../src/business/authenticated-user";
+import { AuditLogService } from "../../src/business/services/audit-log-service";
+import * as edgedb from "edgedb";
+import { registerTypes } from "./setup";
+import { beforeEachCommon } from "./user.common";
+import { addSeconds } from "date-fns";
+
+let existingUser: AuthenticatedUser;
+let auditLogService: AuditLogService;
+let edgeDbClient: edgedb.Client;
+
+beforeEach(async () => {
+  const testContainer = await registerTypes();
+
+  ({ existingUser, edgeDbClient } = await beforeEachCommon());
+
+  auditLogService = testContainer.resolve(AuditLogService);
+});
+
+it("audit user instant", async () => {
+  const start = new Date();
+
+  const aeId = await auditLogService.startUserAuditEvent(
+    edgeDbClient,
+    existingUser.subjectId,
+    existingUser.displayName,
+    existingUser.dbId,
+    "E",
+    "Login",
+    start
+  );
+
+  await auditLogService.completeReleaseAuditEvent(
+    edgeDbClient,
+    aeId,
+    0,
+    start,
+    new Date(),
+    {
+      message: "Message",
+    }
+  );
+
+  const events = await auditLogService.getUserEntries(
+    edgeDbClient,
+    existingUser,
+    1000,
+    0
+  );
+
+  const auditEvent = events?.data?.find((element) => element.objectId === aeId);
+  expect(auditEvent).toBeDefined();
+  expect(auditEvent).toMatchObject({
+    actionCategory: "E",
+    actionDescription: "Login",
+    whoId: existingUser.subjectId,
+    whoDisplayName: existingUser.displayName,
+    hasDetails: true,
+  });
+
+  console.log(JSON.stringify(events));
+});
+
+it("audit user instant with create function", async () => {
+  const aeId = await auditLogService.createUserAuditEvent(
+    edgeDbClient,
+    existingUser.dbId,
+    existingUser.subjectId,
+    existingUser.displayName,
+    "E",
+    "Login",
+    { message: "Message" },
+    8
+  );
+
+  const events = await auditLogService.getUserEntries(
+    edgeDbClient,
+    existingUser,
+    1000,
+    0
+  );
+
+  const auditEvent = events?.data?.find((element) => element.objectId === aeId);
+  expect(auditEvent).toBeDefined();
+  expect(auditEvent).toMatchObject({
+    actionCategory: "E",
+    actionDescription: "Login",
+    whoId: existingUser.subjectId,
+    whoDisplayName: existingUser.displayName,
+    hasDetails: true,
+  });
+
+  console.log(JSON.stringify(events));
+});
+
+it("audit user duration", async () => {
+  const start = new Date();
+
+  const aeId = await auditLogService.startUserAuditEvent(
+    edgeDbClient,
+    existingUser.subjectId,
+    existingUser.displayName,
+    existingUser.dbId,
+    "E",
+    "Login",
+    start
+  );
+
+  await auditLogService.completeUserAuditEvent(
+    edgeDbClient,
+    aeId,
+    0,
+    start,
+    addSeconds(start, 96),
+    {
+      message: "Message",
+    }
+  );
+
+  const events = await auditLogService.getUserEntries(
+    edgeDbClient,
+    existingUser,
+    1000,
+    0
+  );
+
+  const auditEvent = events?.data?.find((element) => element.objectId === aeId);
+  expect(auditEvent).toBeDefined();
+  expect(auditEvent).toMatchObject({
+    occurredDuration: "PT1M36S",
+    actionCategory: "E",
+    actionDescription: "Login",
+    whoId: existingUser.subjectId,
+    whoDisplayName: existingUser.displayName,
+    hasDetails: true,
+  });
+
+  console.log(JSON.stringify(events));
+});
