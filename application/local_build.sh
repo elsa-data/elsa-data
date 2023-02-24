2135f0000a10813441c0c7eae2cf9cfeaba28b99
andrewpatto <andrew@patto.net>
2023-02-24 16:24:54 +1100

    Get it to build on PR

--- application/local_build.sh
+++ application/local_build.sh
@@ -1,2 +1,5 @@
 # for a Docker built locally to a developer we don't assert any meaningful version/sha
-docker build -t elsa . --progress plain --build-arg ELSA_DATA_VERSION=local-dev --build-arg GITHUB_SHA=0000000000000000000000000000000000000000
+docker build -t elsa . --progress plain \
+   --build-arg ELSA_DATA_VERSION=local-dev \
+   --build-arg ELSA_DATA_BUILT=$(date -Iseconds) \
+   --build-arg ELSA_DATA_REVISION=0000000000000000000000000000000000000000
