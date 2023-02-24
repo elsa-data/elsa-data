48a3890acc36443cca495de1277e6045aa55ebf1
andrewpatto <andrew@patto.net>
2023-02-24 14:38:05 +1100

    Make build params mandatory Use git token credentials rather than personal ones

--- application/local_build.sh
+++ application/local_build.sh
@@ -1,1 +1,2 @@
-docker build -t elsa . --progress plain
+# for a Docker built locally to a developer we don't assert any meaningful version/sha
+docker build -t elsa . --progress plain --build-arg ELSA_DATA_VERSION=local-dev --build-arg GITHUB_SHA=0000000000000000000000000000000000000000
