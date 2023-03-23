b0c1f074eee11a9e9fc9c8ea7d5dd9be8ac83b4d
William Putra Intan <61998484+williamputraintan@users.noreply.github.com>
2023-03-23 19:48:06 +1100

    Update example.spec.ts

--- e2e/tests/example.spec.ts
+++ e2e/tests/example.spec.ts
@@ -1,22 +1,22 @@
 import { test, expect } from "@playwright/test";
 
-// It seems it does not pass in GH Action lately, and would disable this for now.
-// To be re-instantiate with issue #232
-// Ref: https://github.com/umccr/elsa-data/issues/232
+test("homepage has title and links to intro page", async ({ page }) => {
+  await page.goto("./");
 
-// test("homepage has title and links to intro page", async ({ page }) => {
-//   await page.goto("./");
+  // expect the title
+  await expect(page).toHaveTitle(/Elsa Data/);
 
-//   // expect the title
-//   await expect(page).toHaveTitle(/Elsa Data/);
+  // because we are not logged in we get some text to show Releases
+  await expect(page.getByRole("heading", { name: "Releases" })).toBeVisible();
 
-//   // because we are not logged in we get some text to show Releases
-//   await expect(page.getByRole("heading", { name: "Releases" })).toBeVisible();
+  // It seems it does not pass in GH Action lately, and would disable this for now.
+  // To be re-instantiate with issue #232
+  // Ref: https://github.com/umccr/elsa-data/issues/232
 
-//   // navigate to a different page
-//   const dacLink = page.getByRole("link", { name: /DAC/ });
+  // navigate to a different page
+  // const dacLink = page.getByRole("link", { name: /DAC/ });
 
-//   await dacLink.click();
+  // await dacLink.click();
 
-//   await expect(page).toHaveURL(/.*dac/);
-// });
+  // await expect(page).toHaveURL(/.*dac/);
+});
