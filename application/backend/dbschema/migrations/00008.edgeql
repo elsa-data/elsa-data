aa95153f13d94876e7ff5b6d3cad9f226cc8680a
Andrew Patterson <andrew@patto.net>
2023-04-06 16:25:11 +1000

    Just reset the migrations given we have no deployments (#280)

--- application/backend/dbschema/migrations/00008.edgeql
+++
@@ -1,7 +0,0 @@
-CREATE MIGRATION m1kam4c6otwvcv4io5ye3x3hf7hknqwdbv7tnrp2b2ienujrc6pj5a
-    ONTO m17u5wr5453fimt7vkqsiekedfwybejyj3syjyjhqygvukrhaduw6a
-{
-  ALTER TYPE release::Release {
-      CREATE PROPERTY lastDateTimeDataAccessLogQuery -> std::datetime;
-  };
-};
--- application/backend/edgedb-start-from-scratch.sh
+++ application/backend/edgedb-start-from-scratch.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 edgedb instance destroy -I elsadata --force
-rm dbschema/migrations/0000*
+rm dbschema/migrations/000*
 edgedb instance create elsadata
 edgedb project init --non-interactive --link --server-instance elsadata
 edgedb migration create --non-interactive
