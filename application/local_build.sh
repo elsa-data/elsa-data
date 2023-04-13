18b02e0b72341406890d3e6da2a9e2f24b20296c
andrewpatto <andrew@patto.net>
2023-04-13 16:14:47 +1000

    Final tidies

--- application/local_build.sh
+++ application/local_build.sh
@@ -1,5 +1,5 @@
 # for a Docker built locally to a developer we don't assert any meaningful version/sha
 docker build -t elsa . --progress plain \
    --build-arg ELSA_DATA_VERSION=local-dev \
-   --build-arg ELSA_DATA_BUILT=$(date -Iseconds) \
+   --build-arg ELSA_DATA_BUILT="$(date -Iseconds)" \
    --build-arg ELSA_DATA_REVISION=0000000000000000000000000000000000000000
