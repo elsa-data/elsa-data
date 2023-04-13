9556f0394469792b5d0d44ef4095985a038eb4be
andrewpatto <andrew@patto.net>
2023-04-13 17:48:50 +1000

    Revert to debian as the edgedb needs to be run directly by some endpoints

--- application/backend/bundle-build.js
+++ application/backend/bundle-build.js
@@ -1,14 +1,16 @@
-// General usage
+// the bundler script to be used only for building the code in the Docker image
 const { build } = require("esbuild");
 const esbuildPluginPino = require("esbuild-plugin-pino");
 
 build({
+  platform: "node",
+  target: "node18",
   entryPoints: ["src/entrypoint.ts", "jobs/entrypoint-job-handler.ts"],
   external: ["sodium-native", "node-gyp-build"],
   bundle: true,
+  // NOTE that this path gets built into some of the pino code so structurally
+  // needs to be retained when running
   outdir: "server/dist",
-  platform: "node",
-  minify: true,
+  minify: false,
   plugins: [esbuildPluginPino({ transports: ["pino-pretty"] })],
-  target: "node18",
 }).catch(() => process.exit(1));
