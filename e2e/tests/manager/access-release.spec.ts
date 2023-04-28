0f944b3dd1eaf194ce5eb024d017624e4532fc0e
William Putra Intan <61998484+williamputraintan@users.noreply.github.com>
2023-04-28 15:14:29 +1000

    check https in zip objects

--- e2e/tests/manager/access-release.spec.ts
+++ e2e/tests/manager/access-release.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect } from "@playwright/test";
 import { managerSetup } from "./manager.common";
+import fs from "fs";
+import Seven from "node-7z";
 
 const RELEASE_KEY = "R001";
 
@@ -8,6 +10,8 @@ test.beforeEach(async ({ page }) => {
 });
 
 test("Download AWS PresignedUrl ZIP files", async ({ page }) => {
+  const dirPath = "./test-results/download/test-aws-presigned/";
+
   await page.locator(`#button-view-${RELEASE_KEY}`).click();
   await page.getByText("Object Signing").click();
 
@@ -15,7 +19,25 @@ test("Download AWS PresignedUrl ZIP files", async ({ page }) => {
   await page.getByText("Download Zip").click();
   const download = await downloadPromise;
 
-  expect(download.suggestedFilename()).toBe(`manifest-${RELEASE_KEY}.zip`);
+  const suggestedFilename = download.suggestedFilename();
+  expect(suggestedFilename).toBe(`manifest-${RELEASE_KEY}.zip`);
+  const zipPath = `${dirPath}${suggestedFilename}`;
+  await download.saveAs(zipPath);
+
+  const stream = Seven.extract(zipPath, dirPath, {
+    password: "abcd",
+  });
+  stream.on("data", (ex: { file: string } & Record<string, string>) => {
+    const extractedPath = `${dirPath}${ex.file}`;
+
+    const manifestData = fs.readFileSync(extractedPath, {
+      encoding: "utf8",
+      flag: "r",
+    });
+    expect(manifestData).toMatch(
+      /https:\/\/umccr-10f-data-dev.s3.ap-southeast-2.amazonaws.com\//i
+    );
+  });
 });
 
 test("Download metadata manifest ZIP file", async ({ page }) => {
