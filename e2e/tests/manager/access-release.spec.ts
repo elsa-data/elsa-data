9e66775fcc4e1f18baff1d014ecdb61307c62048
William Putra Intan <61998484+williamputraintan@users.noreply.github.com>
2023-04-28 13:49:08 +1000

    Manfiest and presigned

--- e2e/tests/manager/access-release.spec.ts
+++ e2e/tests/manager/access-release.spec.ts
@@ -3,12 +3,24 @@ import { managerSetup } from "./manager.common";
 
 const RELEASE_KEY = "R001";
 
-test("Download AWS manifest ZIP", async ({ page }) => {
-  test.slow();
+test.beforeEach(async ({ page }) => {
   await managerSetup(page);
+});
 
+test("Download AWS PresignedUrl ZIP files", async ({ page }) => {
   await page.locator(`#button-view-${RELEASE_KEY}`).click();
   await page.getByText("Object Signing").click();
+
+  const downloadPromise = page.waitForEvent("download");
+  await page.getByText("Download Zip").click();
+  const download = await downloadPromise;
+
+  expect(download.suggestedFilename()).toBe(`manifest-${RELEASE_KEY}.zip`);
+});
+
+test("Download metadata manifest ZIP file", async ({ page }) => {
+  await page.locator(`#button-view-${RELEASE_KEY}`).click();
+  await page.getByRole("tab", { name: "Manifest" }).click();
 
   const downloadPromise = page.waitForEvent("download");
   await page.getByText("Download Zip").click();
