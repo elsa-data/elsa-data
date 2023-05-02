d339868293352350ed709f4882febf8178384589
Andrew Patterson <andrew@patto.net>
2023-05-02 15:26:29 +1000

    Turns out jsonpath can't be bundled - so need to manually deploy its dependency tree (#308)

--- application/local_run.sh
+++ application/local_run.sh
@@ -4,7 +4,7 @@
         -e AWS_SECRET_ACCESS_KEY \
         -e AWS_SESSION_TOKEN \
         -e NODE_ENV="development" \
-        -e ELSA_DATA_META_CONFIG_SOURCES="file('base') file('dev-common') file('dev-localhost') file('datasets') aws-secret('ElsaDataLocalhost')" \
+        -e ELSA_DATA_META_CONFIG_SOURCES="aws-secret('ElsaDataLocalhost')" \
         -e EDGEDB_CLIENT_SECURITY \
         -e EDGEDB_DSN \
         -p 3000:80 elsa)
