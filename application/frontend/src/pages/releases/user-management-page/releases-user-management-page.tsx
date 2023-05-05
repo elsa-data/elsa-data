c1231037ee6f29eda9a76cb7c819643df520bf4f
andrewpatto <andrew@patto.net>
2023-05-05 16:13:31 +1000

    Small fixes to user management page

--- application/frontend/src/pages/releases/user-management-page/releases-user-management-page.tsx
+++ application/frontend/src/pages/releases/user-management-page/releases-user-management-page.tsx
@@ -43,7 +43,10 @@ export const ReleasesUserManagementPage: React.FC = () => {
   };
 
   const addUserMutate = useMutation(
-    (c: { newUserEmail: string; newUserRole: "Manager" | "Member" }) =>
+    (c: {
+      newUserEmail: string;
+      newUserRole: "Manager" | "Member" | "Administrator";
+    }) =>
       axios.post<void>(`/api/releases/${releaseKey}/participants`, {
         email: c.newUserEmail,
         role: c.newUserRole,
@@ -60,17 +63,49 @@ export const ReleasesUserManagementPage: React.FC = () => {
   );
 
   const [newUserEmail, setNewUserEmail] = useState("");
-  const [newUserRoleIsPi, setNewUserRoleIsPi] = useState(false);
+  const [newUserRole, setNewUserRole] = useState<
+    "Member" | "Manager" | "Administrator"
+  >("Member");
 
   const isAddButtonDisabled =
     addUserMutate.isLoading ||
     removeUserMutate.isLoading ||
     newUserEmail.trim().length == 0;
 
+  const ourRadio = (text: string, checked: boolean, onChange: () => void) => (
+    <div className="form-control items-start">
+      <label className="label cursor-pointer">
+        <input
+          type="radio"
+          name="roleRadio"
+          className="radio-accent radio radio-sm mr-2"
+          checked={checked}
+          onChange={onChange}
+        />
+        <span className="label-text">{text}</span>
+      </label>
+    </div>
+  );
+
   return (
     <>
       <Box heading="Add/Update User In This Release">
-        <form className="prose-xs prose max-w-xs">
+        <form className="prose-xs prose max-w-2xl">
+          <p>
+            New users can be invited into a release by entering their email
+            here. Until they log in for the first time the system will only know
+            their email address (after that, it will be able to refer to them by
+            name).
+          </p>
+          <p>
+            Existing users can have their permissions altered by entering their
+            email and setting their new permission level.
+          </p>
+          <p>
+            It is only possible to add/update a user in this release with a
+            level that is one below your own current level in the release. (TO
+            BE IMPLEMENTED)
+          </p>
           <div className="form-control">
             <label className="label">
               <span className="label-text">User Email</span>
@@ -78,36 +113,21 @@ export const ReleasesUserManagementPage: React.FC = () => {
             <input
               type="text"
               placeholder="email@address"
-              className="input-bordered input w-full"
+              className="input-bordered input-accent input input-sm w-full"
               value={newUserEmail}
               onChange={(e) => setNewUserEmail(e.target.value)}
             />
           </div>
-          <div className="form-control">
-            <label className="label cursor-pointer">
-              <span className="label-text">Member</span>
-              <input
-                type="radio"
-                name="roleRadio"
-                className="radio"
-                checked={!newUserRoleIsPi}
-                onChange={(e) => setNewUserRoleIsPi(!newUserRoleIsPi)}
-              />
-            </label>
-          </div>
-          <div className="form-control">
-            <label className="label cursor-pointer">
-              <span className="label-text">Manager</span>
-              <input
-                type="radio"
-                name="roleRadio"
-                className="radio"
-                checked={newUserRoleIsPi}
-                onChange={(e) => setNewUserRoleIsPi(!newUserRoleIsPi)}
-              />
-            </label>
-          </div>
-          <div className="form-control">
+          {ourRadio("Member", newUserRole === "Member", () =>
+            setNewUserRole("Member")
+          )}
+          {ourRadio("Manager", newUserRole === "Manager", () =>
+            setNewUserRole("Manager")
+          )}
+          {ourRadio("Administrator", newUserRole === "Administrator", () =>
+            setNewUserRole("Administrator")
+          )}
+          <div className="form-control max-w-xs">
             <button
               type="button"
               disabled={isAddButtonDisabled}
@@ -117,11 +137,11 @@ export const ReleasesUserManagementPage: React.FC = () => {
               onClick={async () => {
                 addUserMutate.mutate({
                   newUserEmail: newUserEmail,
-                  newUserRole: newUserRoleIsPi ? "Manager" : "Member",
+                  newUserRole: newUserRole,
                 });
               }}
             >
-              Add
+              Add / Update
             </button>
           </div>
         </form>
@@ -137,10 +157,10 @@ export const ReleasesUserManagementPage: React.FC = () => {
               )}
             {releaseParticipantsQuery.data &&
               releaseParticipantsQuery.data.length > 0 && (
-                <table className="table-compact table w-full">
+                <table className="table w-full table-auto">
                   <thead>
                     <tr>
-                      <th>Name</th>
+                      <th scope="col">Name</th>
                       <th>Role</th>
                       <th>Last Login</th>
                       <th></th>
