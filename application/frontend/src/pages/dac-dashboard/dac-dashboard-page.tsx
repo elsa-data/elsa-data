56c4155b9046ecd41bdf5c504741a225ff3a9a45
andrewpatto <andrew@patto.net>
2023-05-09 18:20:42 +1000

    Dialog per

--- application/frontend/src/pages/dac-dashboard/dac-dashboard-page.tsx
+++ application/frontend/src/pages/dac-dashboard/dac-dashboard-page.tsx
@@ -3,11 +3,15 @@ import { Box } from "../../components/boxes";
 import { VerticalTabs } from "../../components/vertical-tabs";
 import { ReleasesAddReleaseDialog } from "./rems-dac/releases-dashboard-add-release-dialog";
 import { ReleasesManualEntryDialog } from "./manual/releases-dashboard-manual-entry-dialog";
-import { useUiAllowed } from "../../hooks/ui-allowed";
-import { ALLOWED_CREATE_NEW_RELEASE } from "@umccr/elsa-constants";
 import { AustralianGenomicsDacRedcapUploadDiv } from "./australian-genomics-dac-redcap/australian-genomics-dac-redcap-upload-div";
 import { trpc } from "../../helpers/trpc";
 
+/**
+ * A dashboard showing the available upstream DAC stacks from which we can source
+ * applications to create new releases.
+ *
+ * @constructor
+ */
 export const DacDashboardPage: React.FC = () => {
   const [showingRemsDialog, setShowingRemsDialog] = useState(false);
   const [showingManualEntryDialog, setShowingManualEntryDialog] =
@@ -21,65 +25,74 @@ export const DacDashboardPage: React.FC = () => {
   return (
     <>
       {dacQuery.isSuccess && (
-        <Box
-          heading="DAC"
-          errorMessage={"Something went wrong loading DAC instances"}
-        >
-          <VerticalTabs tabHeadings={dacQuery.data.map((a) => a.description)}>
-            {dacQuery.data.map((a) => {
-              if (a.type === "rems")
-                return (
-                  <div className="flex flex-col gap-6">
-                    <div className="prose">
-                      <label className="block">
-                        <span className="text-xs font-bold uppercase text-gray-700">
-                          REMS Instance URL
-                        </span>
-                        <input
-                          type="text"
-                          defaultValue={a.url}
-                          className="mt-1 block w-full rounded-md border-transparent bg-gray-50 focus:border-gray-500 focus:bg-white focus:ring-0"
-                        />
-                      </label>
-                      <button
-                        className="btn-normal"
-                        onClick={() => setShowingRemsDialog(true)}
-                      >
-                        Find New Applications
-                      </button>
+        <>
+          <Box
+            heading="DAC"
+            errorMessage={"Something went wrong loading DAC instances"}
+          >
+            <VerticalTabs tabHeadings={dacQuery.data.map((a) => a.description)}>
+              {dacQuery.data.map((a) => {
+                if (a.type === "rems")
+                  return (
+                    <div className="flex flex-col gap-6">
+                      <div className="prose">
+                        <label className="block">
+                          <span className="text-xs font-bold uppercase text-gray-700">
+                            REMS Instance URL
+                          </span>
+                          <input
+                            type="text"
+                            defaultValue={a.url}
+                            className="mt-1 block w-full rounded-md border-transparent bg-gray-50 focus:border-gray-500 focus:bg-white focus:ring-0"
+                          />
+                        </label>
+                        <button
+                          className="btn-normal"
+                          onClick={() => setShowingRemsDialog(true)}
+                        >
+                          Find New Applications
+                        </button>
+                      </div>
                     </div>
-                  </div>
-                );
+                  );
 
-              if (a.type === "manual")
-                return (
-                  <button
-                    className="btn-normal"
-                    onClick={() => setShowingManualEntryDialog(true)}
-                  >
-                    Create Release Manually
-                  </button>
-                );
+                if (a.type === "manual")
+                  return (
+                    <button
+                      className="btn-normal"
+                      onClick={() => setShowingManualEntryDialog(true)}
+                    >
+                      Create Release Manually
+                    </button>
+                  );
 
-              if (
-                a.type === "redcap-australian-genomics-csv" ||
-                a.type === "redcap-australian-genomics-demo-csv"
-              )
-                return <AustralianGenomicsDacRedcapUploadDiv />;
-            })}
-          </VerticalTabs>
-        </Box>
+                if (
+                  a.type === "redcap-australian-genomics-csv" ||
+                  a.type === "redcap-australian-genomics-demo-csv"
+                )
+                  return <AustralianGenomicsDacRedcapUploadDiv />;
+              })}
+            </VerticalTabs>
+          </Box>
+          {dacQuery.data.map((a) => {
+            if (a.type === "rems")
+              return (
+                <ReleasesAddReleaseDialog
+                  showing={showingRemsDialog}
+                  cancelShowing={() => setShowingRemsDialog(false)}
+                />
+              );
+
+            if (a.type === "manual")
+              return (
+                <ReleasesManualEntryDialog
+                  showing={showingManualEntryDialog}
+                  cancelShowing={() => setShowingManualEntryDialog(false)}
+                />
+              );
+          })}
+        </>
       )}
-
-      {/* Dialog component from buttons above */}
-      <ReleasesAddReleaseDialog
-        showing={showingRemsDialog}
-        cancelShowing={() => setShowingRemsDialog(false)}
-      />
-      <ReleasesManualEntryDialog
-        showing={showingManualEntryDialog}
-        cancelShowing={() => setShowingManualEntryDialog(false)}
-      />
     </>
   );
 };
