af84351d02ea24a8ee5c94cd8399a96fad387fdd
Marko Malenic <mmalenic1@gmail.com>
2023-05-29 14:34:01 +1000

    refactor(frontend): tidy audit event table (#351)
    * refactor(frontend): tidy audit event table, remove drop-down menu and add toggle for all events
    
    * refactor(frontend): remove system events checkbox, move permission check outside of filter elements component

--- application/frontend/src/pages/releases/audit-events-sub-page/audit-events-sub-page.tsx
+++ application/frontend/src/pages/releases/audit-events-sub-page/audit-events-sub-page.tsx
@@ -2,20 +2,25 @@ import React from "react";
 import { usePageSizer } from "../../../hooks/page-sizer";
 import { AuditEventTable } from "../../../components/audit-event/audit-event-table";
 import { useParams } from "react-router-dom";
+import { useUiAllowed } from "../../../hooks/ui-allowed";
+import { ALLOWED_OVERALL_ADMIN_VIEW } from "@umccr/elsa-constants";
 
 export const AuditEventsSubPage = () => {
   const pageSize = usePageSizer();
 
   const { releaseKey } = useParams<{ releaseKey: string }>();
 
+  const allowed = useUiAllowed();
+
   return (
     <>
       <AuditEventTable
         path="releases"
         id={releaseKey}
-        filterMenu={false}
-        filterMenuInitial={["release"]}
+        filterElements={false}
+        filterElementsInitial={["release"]}
         pageSize={pageSize}
+        showAdminView={allowed.has(ALLOWED_OVERALL_ADMIN_VIEW)}
       />
     </>
   );
