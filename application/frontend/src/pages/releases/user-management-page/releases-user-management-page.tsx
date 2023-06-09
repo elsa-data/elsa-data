85ace7f41d40d2f1367debaf4de4f68e05338b94
William Putra Intan <61998484+williamputraintan@users.noreply.github.com>
2023-06-10 01:16:37 +1000

    some css

--- application/frontend/src/pages/releases/user-management-page/releases-user-management-page.tsx
+++ application/frontend/src/pages/releases/user-management-page/releases-user-management-page.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { useQueryClient } from "@tanstack/react-query";
 import { EagerErrorBoundary, ErrorState } from "../../../components/errors";
 import { Box } from "../../../components/boxes";
 import { ReleaseParticipantRoleType } from "@umccr/elsa-types";
@@ -9,7 +8,6 @@ import { formatLocalDateTime } from "../../../helpers/datetime-helper";
 import { useReleasesMasterData } from "../releases-types";
 import { Table } from "../../../components/tables";
 import { trpc } from "../../../helpers/trpc";
-import { REACT_QUERY_RELEASE_KEYS } from "../queries";
 
 function checkEmail(email: string) {
   return /\S+@\S+\.\S+/.test(email);
@@ -19,13 +17,11 @@ function checkEmail(email: string) {
  * A page allowing the display/editing of users participating in a release.
  */
 export const ReleasesUserManagementPage: React.FC = () => {
-  const queryClient = useQueryClient();
   const { releaseKey } = useReleasesMasterData();
+  const utils = trpc.useContext();
 
   const afterMutateForceRefresh = () => {
-    return queryClient.invalidateQueries(
-      REACT_QUERY_RELEASE_KEYS.participant(releaseKey)
-    );
+    utils.releaseParticipant.getParticipants.invalidate();
   };
 
   const releaseParticipantsQuery =
@@ -81,75 +77,82 @@ export const ReleasesUserManagementPage: React.FC = () => {
   return (
     <>
       <Box heading="Add/Update User In This Release">
-        <form className="prose-xs prose max-w-2xl">
-          <p>
-            New users can be invited into a release by entering their email
-            here. Until they log in for the first time the system will only know
-            their email address (after that, it will be able to refer to them by
-            name).
-          </p>
-          <p>
-            Existing users can have their permissions altered by entering their
-            email and setting their new permission level.
-          </p>
-          <p>
-            It is only possible to add/update a user in this release with a
-            level that is one below your own current level in the release. (TO
-            BE IMPLEMENTED)
-          </p>
-          <div className="form-control mb-3">
-            <label className="label">
-              <span className="label-text">User Email</span>
-            </label>
-            <input
-              type="email"
-              placeholder="user@email.com"
-              className={classNames(
-                `input-bordered input-accent input input-sm w-full`,
-                {
-                  "input-error": !isValidEmail,
-                  "input-accent": isValidEmail,
-                }
+        <div className="flex w-full flex-col sm:flex-row">
+          <div className="card prose rounded-box grid flex-grow prose-p:space-y-2 sm:w-1/2">
+            <p>
+              New users can be invited into a release by entering their email
+              here. Until they log in for the first time the system will only
+              know their email address (after that, it will be able to refer to
+              them by name).
+            </p>
+            <p>
+              Existing users can have their permissions altered by entering
+              their email and setting their new permission level.
+            </p>
+            <p>
+              It is only possible to add/update a user in this release with a
+              level that is one below your own current level in the release. (TO
+              BE IMPLEMENTED)
+            </p>
+          </div>
+          <div className="divider divider-vertical sm:divider-horizontal" />
+          <div className="card rounded-box flex-grow justify-between">
+            <div>
+              <div className="form-control mb-2">
+                <label className="label">
+                  <span className="label-text">User Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="user@email.com"
+                  className={classNames(
+                    `input-bordered input-accent input input-sm w-full`,
+                    {
+                      "input-error": !isValidEmail,
+                      "input-accent": isValidEmail,
+                    }
+                  )}
+                  value={newUserEmail}
+                  onChange={(e) => setNewUserEmail(e.target.value)}
+                />
+                {!isValidEmail && (
+                  <label className="label">
+                    <span className="label-text-alt text-red-400">
+                      Invalid email
+                    </span>
+                  </label>
+                )}
+              </div>
+              {ourRadio("Member", newUserRole === "Member", () =>
+                setNewUserRole("Member")
               )}
-              value={newUserEmail}
-              onChange={(e) => setNewUserEmail(e.target.value)}
-            />
-            {!isValidEmail && (
-              <label className="label">
-                <span className="label-text-alt text-red-400">
-                  Email invalid
-                </span>
-              </label>
-            )}
+              {ourRadio("Manager", newUserRole === "Manager", () =>
+                setNewUserRole("Manager")
+              )}
+              {ourRadio("Administrator", newUserRole === "Administrator", () =>
+                setNewUserRole("Administrator")
+              )}
+            </div>
+            <div className="form-control my-2 max-w-full">
+              <button
+                type="button"
+                disabled={isAddButtonDisabled}
+                className={classNames("btn", {
+                  "btn-disabled": isAddButtonDisabled,
+                })}
+                onClick={() => {
+                  addParticipantMutate.mutate({
+                    releaseKey,
+                    email: newUserEmail,
+                    role: newUserRole,
+                  });
+                }}
+              >
+                Add
+              </button>
+            </div>
           </div>
-          {ourRadio("Member", newUserRole === "Member", () =>
-            setNewUserRole("Member")
-          )}
-          {ourRadio("Manager", newUserRole === "Manager", () =>
-            setNewUserRole("Manager")
-          )}
-          {ourRadio("Administrator", newUserRole === "Administrator", () =>
-            setNewUserRole("Administrator")
-          )}
-          <div className="form-control max-w-xs">
-            <button
-              type="button"
-              disabled={isAddButtonDisabled}
-              className={classNames("btn", {
-                "btn-disabled": isAddButtonDisabled,
-              })}
-              onClick={() => {
-                addParticipantMutate.mutate({
-                  releaseKey,
-                  email: newUserEmail,
-                  role: newUserRole,
-                });
-              }}
-            >
-              Add / Update
-            </button>
-          </div>
-        </form>
+        </div>
       </Box>
 
       {releaseParticipantsQuery.isSuccess && (
