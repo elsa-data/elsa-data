e9230c09f19df13c5a27acf420c5a6ac13a1ba41
andrewpatto <andrew@patto.net>
2023-07-10 15:22:54 +1000

    Small micro fixes for researcher view Access methods should be disabled based on presence of config AND of whether they are enabled (this is different logic than for Admins)

--- application/frontend/src/pages/releases/detail/access-box/object-signing-form.tsx
+++ application/frontend/src/pages/releases/detail/access-box/object-signing-form.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import classNames from "classnames";
 import { CSRFInputToken } from "../../../../components/csrf-token";
 import { TsvColumnCheck } from "../../../../components/access-box";
 import { ReleaseTypeLocal } from "../../shared-types";
@@ -7,10 +6,12 @@ import { ObjectStoreRecordKey } from "@umccr/elsa-types/schemas";
 import { Alert } from "../../../../components/alert";
 import { trpc } from "../../../../helpers/trpc";
 import { EagerErrorBoundary } from "../../../../components/errors";
+import { SharerObjectSigningType } from "../../../../../../backend/src/config/config-schema-sharer";
 
 type Props = {
   releaseKey: string;
   releaseData: ReleaseTypeLocal;
+  objectSigningSetting: SharerObjectSigningType;
 };
 
 export const ObjectSigningForm: React.FC<Props> = ({
@@ -32,7 +33,7 @@ export const ObjectSigningForm: React.FC<Props> = ({
       {isPrepareDownload && (
         <Alert
           icon={<span className="loading loading-bars loading-xs" />}
-          description={"Preparing signed ZIP file for download"}
+          description={"Preparing ZIP file for download"}
           additionalAlertClassName={
             "alert alert-info bg-slate-300 text-md py-1 mb-3"
           }
@@ -69,8 +70,10 @@ export const ObjectSigningForm: React.FC<Props> = ({
             ) : getFilePasswordMutate.data ? (
               <button
                 className="btn-table-action-navigate"
-                onClick={() => {
-                  navigator.clipboard.writeText(getFilePasswordMutate.data!);
+                onClick={async () => {
+                  await navigator.clipboard.writeText(
+                    getFilePasswordMutate.data!
+                  );
                   setIsPassInClipboard(true);
                   setTimeout(() => {
                     setIsPassInClipboard(false);
@@ -113,6 +116,7 @@ export const ObjectSigningForm: React.FC<Props> = ({
                 the given genomic file.
               </p>
             </article>
+
             {ObjectStoreRecordKey.map((field, i) => (
               <TsvColumnCheck key={i} field={field} />
             ))}
@@ -122,10 +126,7 @@ export const ObjectSigningForm: React.FC<Props> = ({
                 type="submit"
                 disabled={!releaseData.activation}
                 className="btn-normal mt-4"
-                value={
-                  "Download Zip" +
-                  (!releaseData.activation ? " (Release Must Be Active)" : "")
-                }
+                value={"Download Zip"}
                 onClick={() => {
                   // Set loader alert
                   setIsPrepareDownload(true);
