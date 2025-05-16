bb73ebe2feee74f543cae65e38f8bfa691946027
andrewpatto <andrew@patto.net>
2025-05-16 15:48:22 +1000

    We don't do dark mode elsewhere so shouldn't in footer

--- application/frontend/src/layouts/layout-base-footer.tsx
+++ application/frontend/src/layouts/layout-base-footer.tsx
@@ -7,8 +7,8 @@ export const LayoutBaseFooter: React.FC = () => {
   return (
     <>
       <div className="container mx-auto w-full">
-        <footer className="p-4 dark:bg-gray-800 md:flex md:items-center md:justify-between md:p-6">
-          <span className="text-sm text-gray-500 dark:text-gray-400 sm:text-center">
+        <footer className="p-4 md:flex md:items-center md:justify-between md:p-6">
+          <span className="text-sm text-gray-500 sm:text-center">
             Elsa Data
             <span
               className="mr-1 ml-3 text-gray-300"
@@ -18,10 +18,10 @@ export const LayoutBaseFooter: React.FC = () => {
             </span>
             {envRelay.version}
           </span>
-          <ul className="mt-3 flex flex-wrap items-center text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+          <ul className="mt-3 flex flex-wrap items-center text-sm text-gray-500 sm:mt-0">
             <li>
               <a
-                href="https://github.com/umccr/elsa-data"
+                href="https://github.com/elsa-data/elsa-data"
                 className="hover:underline"
               >
                 GitHub
