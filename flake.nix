9bd2a506fc5e94d4c94a055c20f71a1631078812
ignatiusm <ignatius.menzies@gmail.com>
2026-05-07 16:53:15 +1200

    Add temporary dockerised gel

--- flake.nix
+++ flake.nix
@@ -1,7 +1,8 @@
 {
   inputs = {
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs/3492680c7307336670aa778f4ff796459d4f24a6";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
+    # nixpkgs.url = "github:NixOS/nixpkgs/3492680c7307336670aa778f4ff796459d4f24a6";
     systems.url = "github:nix-systems/default";
   };
 
@@ -26,12 +27,25 @@
 
           # You can choose pnpm, yarn, or none (npm).
           #pkgs.nodePackages.pnpm
-          pkgs.yarn-berry
+          # pkgs.yarn-berry
 
-          pkgs.nodePackages.typescript
-          pkgs.nodePackages.typescript-language-server
-          pkgs.edgedb
+          pkgs.typescript
+          pkgs.typescript-language-server
+          # pkgs.nodepackages.typescript
+          # pkgs.nodepackages.typescript-language-server
+          # pkgs.edgedb
+          pkgs.k3d
+          pkgs.bun
+          # pkgs.gel
+          (pkgs.writeShellScriptBin "gel" ''
+           exec docker run --rm -i --entrypoint gel --network host -v $(pwd):/app -w /app geldata/gel:7 --dsn gel://admin:development@localhost:5656 --tls-security insecure "$@"
+          '')
         ];
+
+        shellHook = ''
+          export GEL_DSN="gel://admin:development@localhost:5656"
+          export GEL_CLIENT_TLS_SECURITY="insecure"
+        '';
       };
     });
   };
