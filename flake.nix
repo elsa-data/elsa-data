49d5bcefeb879fa757d3c99cabe2862a75b19051
ignatiusm <ignatius.menzies@gmail.com>
2026-05-19 11:24:34 +1200

    Run Gel outside docker

--- flake.nix
+++ flake.nix
@@ -1,8 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
-    # nixpkgs.url = "github:NixOS/nixpkgs/3492680c7307336670aa778f4ff796459d4f24a6";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
     systems.url = "github:nix-systems/default";
   };
 
@@ -14,38 +13,33 @@
     eachSystem = f:
       nixpkgs.lib.genAttrs (import systems) (
         system:
-          f nixpkgs.legacyPackages.${system}
+          f (import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          })
       );
   in {
     devShells = eachSystem (pkgs: {
       default = pkgs.mkShell {
         buildInputs = [
-          #pkgs.nodejs
-          # You can set the major version of Node.js to a specific one instead
-          # of the default version
           pkgs.nodejs_20
-
-          # You can choose pnpm, yarn, or none (npm).
-          #pkgs.nodePackages.pnpm
-          # pkgs.yarn-berry
 
           pkgs.typescript
           pkgs.typescript-language-server
-          # pkgs.nodepackages.typescript
-          # pkgs.nodepackages.typescript-language-server
           # pkgs.edgedb
           pkgs.k3d
           pkgs.bun
-          # pkgs.gel
-          (pkgs.writeShellScriptBin "gel" ''
-           exec docker run --rm -i --entrypoint gel --network host -v $(pwd):/app -w /app geldata/gel:7 --dsn gel://admin:development@localhost:5656 --tls-security insecure "$@"
-          '')
+          pkgs.gel
+          # Alias and shell hook are just for running gel in docker if required
+          # (pkgs.writeShellScriptBin "gel" ''
+           # exec docker run --rm -i --entrypoint gel --network host -v $(pwd):/app -w /app geldata/gel:7 --dsn gel://admin:development@localhost:5656 --tls-security insecure "$@"
+          # '')
         ];
 
-        shellHook = ''
-          export GEL_DSN="gel://admin:development@localhost:5656"
-          export GEL_CLIENT_TLS_SECURITY="insecure"
-        '';
+        # shellHook = ''
+        #   export GEL_DSN="gel://admin:development@localhost:5656"
+        #   export GEL_CLIENT_TLS_SECURITY="insecure"
+        # '';
       };
     });
   };
